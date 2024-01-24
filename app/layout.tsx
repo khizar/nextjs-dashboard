ccaa45f4ae06e80245bd1d9ff082b3135bdccce8
khizar <khizar.aziz@gmail.com>
2024-01-24 17:17:01 +0100

    complete tutorial

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,15 @@
+import { Metadata } from 'next';
 import '@/app/ui/global.css';
 import { inter } from '@/app/ui/fonts';
+
+export const metadata: Metadata = {
+  title: {
+    template: '%s | Acme Dashboard',
+    default: 'Acme Dashboard',
+  },
+  description: 'The official Next.js Learn Dashboard built with App Router.',
+  metadataBase: new URL('https://next-learn-dashboard.vercel.sh'),
+};
 
 export default function RootLayout({
   children,
@@ -8,7 +18,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body  className={`${inter.className} antialiased`}>{children}</body>
+      <body className={`${inter.className} antialiased`}>{children}</body>
     </html>
   );
 }
